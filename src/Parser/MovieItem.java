8a6c989642861e55bac8f2b8e567e9f895d2cb8e
Kard1n <kardin37@gmail.com>
2018-01-26 15:20:45 +0200

    Added site parser class, movieItem class.

---
+++ src/Parser/MovieItem.java
@@ -0,0 +1,101 @@
+package Parser;
+
+public class MovieItem {
+
+    private boolean moviePremier;
+    private String movieNameUkr;
+    private String movieNameEng;
+    private String moviePosterUrl;
+    private String movieGenre;
+    private String movieTime;
+    private String moviePremierDay;
+    private String movieYear;
+    private String movieCountry;
+    private String movieActors;
+    private String movieProducer;
+    private String movieScenario;
+    private String movieTechnologies;
+    private String movieYoutubeUrl;
+    private String movieYoutubeImg;
+
+    public MovieItem(boolean moviePremier, String movieNameUkr, String movieNameEng, String moviePosterUrl,
+                     String movieGenre, String movieTime, String moviePremierDay, String movieYear,
+                     String movieCountry, String movieActors, String movieProducer, String movieScenario,
+                     String movieTechnologies, String movieYoutubeUrl, String movieYoutubeImg){
+        this.moviePremier = moviePremier;
+        this.movieNameUkr = movieNameUkr;
+        this.movieNameEng = movieNameEng;
+        this.moviePosterUrl = moviePosterUrl;
+        this.movieGenre = movieGenre;
+        this.movieTime = movieTime;
+        this.moviePremierDay = moviePremierDay;
+        this.movieYear = movieYear;
+        this.movieCountry = movieCountry;
+        this.movieActors = movieActors;
+        this.movieProducer = movieProducer;
+        this.movieScenario = movieScenario;
+        this.movieTechnologies = movieTechnologies;
+        this.movieYoutubeUrl = movieYoutubeUrl;
+        this.movieYoutubeImg = movieYoutubeImg;
+    }
+
+    public boolean isMoviePremier() {
+        return moviePremier;
+    }
+
+    public String getMovieNameUkr() {
+        return movieNameUkr;
+    }
+
+    public String getMovieNameEng() {
+        return movieNameEng;
+    }
+
+    public String getMoviePosterUrl() {
+        return moviePosterUrl;
+    }
+
+    public String getMovieGenre() {
+        return movieGenre;
+    }
+
+    public String getMovieTime() {
+        return movieTime;
+    }
+
+    public String getMoviePremierDay() {
+        return moviePremierDay;
+    }
+
+    public String getMovieYear() {
+        return movieYear;
+    }
+
+    public String getMovieCountry() {
+        return movieCountry;
+    }
+
+    public String getMovieActors() {
+        return movieActors;
+    }
+
+    public String getMovieProducer() {
+        return movieProducer;
+    }
+
+    public String getMovieScenario() {
+        return movieScenario;
+    }
+
+    public String getMovieTechnologies() {
+        return movieTechnologies;
+    }
+
+    public String getMovieYoutubeUrl() {
+        return movieYoutubeUrl;
+    }
+
+    public String getMovieYoutubeImg() {
+        return movieYoutubeImg;
+    }
+}
